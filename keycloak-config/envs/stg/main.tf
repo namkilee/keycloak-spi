7b8fdcb91a1f0ce47b10f205c9f83b613cb20511
Namki Lee <genesis0923@naver.com>
2026-01-26 20:38:52 +0900

    Document bootstrap realm usage for auth realm

--- keycloak-config/envs/stg/main.tf
+++ keycloak-config/envs/stg/main.tf
@@ -30,8 +30,8 @@ locals {
 
 provider "keycloak" {
   url           = var.keycloak_url
-  client_id     = var.keycloak_client_id
-  client_secret = var.keycloak_client_secret
+  client_id     = coalesce(var.keycloak_client_id, data.terraform_remote_state.bootstrap.outputs.terraform_client_id)
+  client_secret = coalesce(var.keycloak_client_secret, data.terraform_remote_state.bootstrap.outputs.terraform_client_secret)
   realm         = local.resolved_auth_realm
 }
 
