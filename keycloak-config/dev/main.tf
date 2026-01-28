0552a47fbe161f100ce12d5e487da5aa445d38f1
Namki Lee <genesis0923@naver.com>
2026-01-28 19:36:46 +0900

    Update main.tf

--- keycloak-config/dev/main.tf
+++ keycloak-config/dev/main.tf
@@ -1,31 +1,8 @@
-data "terraform_remote_state" "bootstrap" {
-  backend = "s3"
-  config = {
-    bucket                      = var.bootstrap_state_bucket
-    key                         = var.bootstrap_state_key
-    region                      = var.bootstrap_state_region
-    endpoint                    = var.bootstrap_state_endpoint
-    access_key                  = var.bootstrap_state_access_key
-    secret_key                  = var.bootstrap_state_secret_key
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-  }
-}
-
 locals {
   resolved_auth_realm = coalesce(
     var.keycloak_auth_realm,
     data.terraform_remote_state.bootstrap.outputs.bootstrap_realm_id
   )
-}
-
-provider "keycloak" {
-  url           = var.keycloak_url
-  client_id     = coalesce(var.keycloak_client_id, data.terraform_remote_state.bootstrap.outputs.terraform_client_id)
-  client_secret = coalesce(var.keycloak_client_secret, data.terraform_remote_state.bootstrap.outputs.terraform_client_secret)
-  realm         = local.resolved_auth_realm
 }
 
 module "realm_clients" {
