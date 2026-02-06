7f82c79632aaf64bf1208214b204bba5a4c78d52
Namki Lee <genesis0923@naver.com>
2026-02-06 16:25:49 +0900

    Update TermsAcceptanceStore.java

--- spi/terms-ra/src/main/java/com/example/keycloak/terms/TermsAcceptanceStore.java
+++ spi/terms-ra/src/main/java/com/example/keycloak/terms/TermsAcceptanceStore.java
@@ -4,11 +4,9 @@ import com.example.keycloak.terms.TermsModels.Term;
 import org.keycloak.models.ClientModel;
 import org.keycloak.models.UserModel;
 
-import java.time.OffsetDateTime;
-
 public class TermsAcceptanceStore {
 
-  // key format: tc.accepted.<clientId>.<termId>.version
+  // key format: tc.accepted.<clientId>.<termKey>
   public boolean isAccepted(UserModel user, ClientModel client, Term term) {
     String acceptedVersion = user.getFirstAttribute(key(client, term));
     return term.version().equals(acceptedVersion);
@@ -16,10 +14,9 @@ public class TermsAcceptanceStore {
 
   public void markAccepted(UserModel user, ClientModel client, Term term) {
     user.setSingleAttribute(key(client, term), term.version());
-    user.setSingleAttribute(key(client, term) + ".at", OffsetDateTime.now().toString());
   }
 
   private String key(ClientModel client, Term term) {
-    return "tc.accepted." + client.getClientId() + "." + term.id() + ".version";
+    return "tc.accepted." + client.getClientId() + "." + term.key();
   }
 }
